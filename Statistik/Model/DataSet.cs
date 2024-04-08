dc38c31ba0a4e57e034e80fabf0875e910dc7483
NekoFerris <nekoandreas@gmail.com>
2024-04-08 15:58:51 +0200

    Projektdateien hinzufügen.

---
+++ Statistik/Model/DataSet.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows.Media;
+
+namespace Statistik.Model
+{
+    internal class DataSet
+    {
+        private string _name;
+        private int _value;
+        private SolidColorBrush _solidColorBrush;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+            }
+        }
+        public SolidColorBrush solidColorBrush
+        {
+            get
+            {
+                return _solidColorBrush;
+            }
+            set
+            {
+                _solidColorBrush = value;
+            }
+        }
+        public byte ColorR
+        {
+            get
+            {
+                return _solidColorBrush.Color.R;
+            }
+            set 
+            {
+                Color color = _solidColorBrush.Color;
+                color.R = value;
+                _solidColorBrush.Color = color;
+            }
+        }
+        public byte ColorG
+        {
+            get
+            {
+                return _solidColorBrush.Color.G;
+            }
+            set
+            {
+                Color color = _solidColorBrush.Color;
+                color.G = value;
+                _solidColorBrush.Color = color;
+            }
+        }
+        public byte ColorB
+        {
+            get
+            {
+                return _solidColorBrush.Color.B;
+            }
+            set
+            {
+                Color color = _solidColorBrush.Color;
+                color.B = value;
+                _solidColorBrush.Color = color;
+            }
+        }
+    }
+}
---
+++ Statistik/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using FWIWebShop;
+using Statistik.Model;
+
+namespace Statistik.ViewModel
+{
+    internal class MainViewModel : BaseModel
+    {
+        private DataSet _selectedDataSet;
+        private List<DataSet> _dataSets = new();
+        public DataSet SelectedDataSet
+        {
+            get
+            {
+                return _selectedDataSet;
+            }
+            set
+            {
+                _selectedDataSet = value;
+                OnPropertyChanged(nameof(SelectedDataSet));
+            }
+        }
+        public List<DataSet> DataSets
+        {
+            get 
+            { 
+                return _dataSets;
+            }
+            set
+            {
+                _dataSets = value;
+                OnPropertyChanged(nameof(DataSets));
+            }
+        }
+    }
+}
